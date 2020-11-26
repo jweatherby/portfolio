0f9f0487310d5f3c5e6d277b03cbfc3efad77332
J Weatherby <jamie.weatherby@gmail.com>
2020-11-25 22:09:42 -0500

    Add matomo tracking

--- src/server.js
+++ src/server.js
@@ -47,7 +47,21 @@ server
         </style>
     </head>
     <body>
-        <div id="root">${markup}</div>
+        <div id="root">${markup}</div><!-- Matomo -->
+        <script type="text/javascript">
+          var _paq = window._paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="https://showboard.matomo.cloud/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '2']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/showboard.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+        </script>
+        <!-- End Matomo Code -->
     </body>
 </html>`
       )
